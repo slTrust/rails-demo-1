a515a9d6247ba0022dae4a0a5000c404945b8ef2
huangjiaxi <huangjiaxi@oak-inc.com>
2020-07-01 11:46:03 +0800

    控制渲染内容,在api模式添加hmlt渲染支持

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::API
+  # 添加html渲染支持
+  include ActionView::Layouts
 end
--- app/controllers/first_controller.rb
+++ app/controllers/first_controller.rb
@@ -1,7 +1,13 @@
 class FirstController < ApplicationController
-    def hello
-      # 渲染 文本
-      render plain:'hello rails'
-    end
+  def hello
+    # 渲染 文本
+    # render plain:'hello rails'
+    # 渲染 json
+    # render json: {name:'hjx',age: 18}
+
+    # 渲染 html  app/views/first/hello.html
+        # app/views 目录新建 first目录
+        # app/views/first目录里新建 hello.html
+    render 'first/hello'
   end
-    
+end
